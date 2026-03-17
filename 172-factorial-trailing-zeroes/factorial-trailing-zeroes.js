8e7be13fa8e53cc9623566cb3a4f15636f31eec2
veera-2225 <veerababu4p4@gmail.com>
2026-03-17 12:42:30 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (80.14%) - LeetSync

---
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var trailingZeroes = function(n) {
+    let count = 0;
+
+    for(let i=5; i<=n; i*=5){
+        count = count + Math.floor(n/i)
+    }
+    return count
+};
