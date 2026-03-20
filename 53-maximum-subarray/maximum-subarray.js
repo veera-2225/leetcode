9e1ed0524e1238a39ca4ad540ecc44206d2437e0
veera-2225 <veerababu4p4@gmail.com>
2026-03-20 22:44:48 +0530

    Time: 1 ms (87.76%) | Memory: 64 MB (57.37%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let max = nums[0];
+    let cur = nums[0];
+    for(let i=1;i<nums.length;i++){
+        cur = Math.max(nums[i], cur + nums[i]);
+        max = Math.max(max, cur)
+    }
+    return max
+};
