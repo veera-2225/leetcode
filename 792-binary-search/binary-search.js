2764df143f828c4f87934456ad88a125a9438b19
veera-2225 <veerababu4p4@gmail.com>
2026-03-22 17:48:31 +0530

    Time: 0 ms (100.00%) | Memory: 58.5 MB (16.95%) - LeetSync

--- 792-binary-search/binary-search.js
+++ 792-binary-search/binary-search.js
@@ -4,7 +4,7 @@
  * @return {number}
  */
 var search = function (nums, target) {
-    if(nums.length === 1 && nums[0] === target) return 0;
+    /* if(nums.length === 1 && nums[0] === target) return 0;
     let start = 0;
     let end = nums.length - 1;
 
@@ -20,5 +20,24 @@ var search = function (nums, target) {
             start = mid + 1;
         }
     }
-    return -1
+    return -1 */
+
+    let low = 0;
+    let high = nums.length - 1;
+
+   function recursive(low, high){
+        if(low > high){
+            return -1
+        }
+        let mid = Math.floor((low + high) / 2);
+
+        if(nums[mid] === target){
+            return mid
+        }else if(nums[mid] > target){
+            return recursive(low, mid - 1)
+        }else if(nums[mid] < target){
+            return recursive(mid + 1, high)
+        } 
+    }
+   return recursive(low, high)
 };
