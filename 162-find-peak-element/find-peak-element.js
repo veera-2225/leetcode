400844c86819772806ab8d3d31135786a6d6e592
veera-2225 <veerababu4p4@gmail.com>
2026-03-23 12:56:25 +0530

    Time: 0 ms (100.00%) | Memory: 55.4 MB (5.51%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function (nums) {
+    if (nums.length === 1) return 0;
+    if (nums.length === 2) {
+        if (nums[0] > nums[1]) {
+            return 0
+        } else {
+            return 1
+        }
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        if (i === 0) {
+            if (nums[i] > 0 && nums[i] > nums[i + 1]) {
+                return i
+            }
+        } if (i === nums.length - 1) {
+            if(nums[i] >= nums[i - 1]){
+                return i
+            }
+         } else if (nums[i] >= nums[i - 1] && nums[i] >= nums[i + 1]) {
+            return i
+        }
+    }
+};
