b82620c0b43db78c7c4a0df08e434e77f4a5d4da
veera-2225 <veerababu4p4@gmail.com>
2026-04-02 11:55:36 +0530

    Time: 1 ms (59.72%) | Memory: 60.1 MB (87.67%) - LeetSync

--- 344-reverse-string/reverse-string.js
+++ 344-reverse-string/reverse-string.js
@@ -6,12 +6,12 @@ var reverseString = function (s) {
     let left = 0;
     let right = s.length - 1;
 
-    while (left <= right) {
+    while (left < right) {
         let temp;
         temp = s[left];
         s[left] = s[right];
         s[right] = temp
-        
+
         left++;
         right--;
     }
