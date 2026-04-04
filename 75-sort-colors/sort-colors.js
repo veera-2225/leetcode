5292179fa31bb81401eab5568de4d595cd23453e
veera-2225 <veerababu4p4@gmail.com>
2026-04-04 20:54:25 +0530

    Time: 1 ms (27.79%) | Memory: 55.6 MB (6.14%) - LeetSync

---
+++ 75-sort-colors/sort-colors.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function (nums) {
+    for (let i = 0; i < nums.length - 1; i++) {
+        let flag = false;
+        for (let j = 0; j < nums.length - i - 1; j++) {
+            if (nums[j] > nums[j + 1]) {
+                [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]];
+                flag = true
+            }
+        }
+        if (!flag) {
+            break;
+        }
+    }
+    return nums
+};
